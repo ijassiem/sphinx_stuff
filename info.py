5caf65db1ce756b5cd5a929ea869aacb52f60733
Ismail <56064610+ijassiem@users.noreply.github.com>
2021-08-11 11:58:56 +0200

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,7 @@
 """ This module contains functions for displaying machine information.
 """
 import socket
+import os
 
 def host_name():
     """This function returns the hostname of computer
@@ -9,3 +10,11 @@ def host_name():
     """
     result = socket.gethostname()
     return c
+
+def machine_os():
+    """This function returns the name of the operating system running on machine
+    :return: the operating system of computer
+    :rtype: str
+    """
+    result = os.name
+    return result
